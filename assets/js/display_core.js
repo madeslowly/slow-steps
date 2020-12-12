ad4a5563bc9164fa34ccd98c4e265e513a0f733c
arrancurran <arrancurran@gmail.com>
2020-12-12 15:10:45 +0000

    cleaning

--- assets/js/display_core.js
+++
@@ -1,8 +0,0 @@
-window.onload = function display_core() {
-
-  var member = document.getElementById('teamMember');
-  var core = member.classList.contains('team--core');
-  core.classList.toggle('.d-none');
-
-  console.log("hello");
-};
