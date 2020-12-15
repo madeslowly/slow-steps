c1ba6baf84fabcb91f968574b28dde2674e17f2f
arrancurran <arrancurran@gmail.com>
2020-12-15 13:56:10 +0000

    ncftp troubles

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,11 +2,6 @@
 
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
-    ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-    rm -rf /public_html/madeslowly.xyz/gaitq/test
-    mkdir /public_html/madeslowly.xyz/gaitq/test
-    quit
-EOF
 
     cd _site || exit
     ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /public_html/madeslowly.xyz/gaitq/test/ .
