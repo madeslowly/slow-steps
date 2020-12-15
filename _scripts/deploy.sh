8fe9cc2fc57d4390066d0ca6e444e7a662616bb6
arrancurran <arrancurran@gmail.com>
2020-12-15 13:14:56 +0000

    path errors

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -3,11 +3,11 @@
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
     ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-    rm -rf /home4/ydnwpmmy/public_html/madeslowly.xyz/gaitq/test
-    mkdir /home4/ydnwpmmy/public_html/madeslowly.xyz/gaitq/test/
+    rm -rf /public_html/madeslowly.xyz/gaitq/test
+    mkdir /public_html/madeslowly.xyz/gaitq/test
     quit
 EOF
 
     cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /home4/ydnwpmmy/public_html/madeslowly.xyz/gaitq/test/ .
+    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /public_html/madeslowly.xyz/gaitq/test/ .
 fi
