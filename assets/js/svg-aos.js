ce585fedbdc9b7e43ab2f3fa92f4b3c928ff0309
arrancurran <arrancurran@gmail.com>
2020-12-20 17:01:54 +0000

    animate svg aug with opacity and dashoffset

--- assets/js/svg-aos.js
+++ assets/js/svg-aos.js
@@ -10,7 +10,33 @@ window.addEventListener("scroll", function(e) {
 
   var view_height = document.documentElement.clientHeight;
 
+  var paths = Array.from(document.getElementsByClassName('svg--line'));
+
+
+  for (let i = 0; i < paths.length; i++) {
+
+    var pathLength = paths[i].getTotalLength();
+
+    // Make very long dashes (the length of the path itself)
+    paths[i].style.strokeDasharray = pathLength + ' ' + pathLength;
+
+    // Offset the dashes so the it appears hidden entirely
+    paths[i].style.strokeDashoffset = - pathLength;
+
+    var abs_path_top = paths[i].getBoundingClientRect().top;
+
+    if (abs_path_top <= .8 * view_height) {
+      paths[i].classList.add('ani-in-draw');
+      //paths[i].style.strokeDashoffset = pathLength * abs_path_top / view_height / .2;
+    } else {
+      paths[i].classList.remove('ani-in-draw');
+    }
+    //console.log(abs_path_top / view_height)
+  }
+
+
   for (let i = 0; i < fades.length; i++) {
+
     var abs_fades_top = fades[i].getBoundingClientRect().top;
 
     if (abs_fades_top <= .8 * view_height) {
