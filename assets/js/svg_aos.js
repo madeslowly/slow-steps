a694fcc35ae65b0af7180755faf7a9b3e650ef33
arrancurran <arrancuran@gmail.com>
2020-12-21 17:32:17 +0000

    animate in/out svg__labels
    throttled onscroll animating in our svg augmentation at 20% in

---
+++ assets/js/svg_aos.js
@@ -0,0 +1,25 @@
+// collect all the circles and bubbles so we can ani them in at 0%
+var view_height = document.documentElement.clientHeight;
+
+var svg_aos = function(e) {
+
+  var svgs = Array.from(document.getElementsByClassName("svg__labels"));
+
+  for (let i = 0; i < svgs.length; i++) {
+
+    var abs_path_top = svgs[i].getBoundingClientRect().top;
+
+    if (abs_path_top <= .8 * view_height) {
+      svgs[i].classList.remove('hidden');
+      svgs[i].classList.add('not-hidden');
+    } else {
+      svgs[i].classList.add('hidden');
+      svgs[i].classList.remove('not-hidden');
+    }
+
+
+  }
+
+};
+
+window.addEventListener('scroll', _.throttle(svg_aos, 100, { leading: true, trailing: true}));
