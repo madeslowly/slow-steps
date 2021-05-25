7b5b27102b9b2a3245e707e2c32e565bb8c040b6
arrancurran <arrancurran@gmail.com>
2021-05-25 21:07:26 +0100

    ignores

--- _scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
-then
-
-    cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /public_html/madeslowly.xyz/gaitq/test/ .
-fi
